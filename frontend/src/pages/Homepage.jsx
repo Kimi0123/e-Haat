e3ec7c62fb5c0e21d1109ca9d7fbb5447d8f7214
Kimi0123 <maharjannirvan01@gmail.com>
2025-06-30 07:48:52 +0545

    removed hompegae code

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,20 +1,10 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import HeroSection from "../components/homepage/HeroSection";
-import Features from "../components/homepage/Features";
-import Categories from "../components/homepage/Categories";
-import HowItWorks from "../components/homepage/HowItWorks";
-import Testimonials from "../components/homepage/Testimonials";
 
 export default function Homepage() {
   return (
     <div className="min-h-screen">
       <Navbar />
-      <HeroSection />
-      <Features />
-      <Categories />
-      <HowItWorks />
-      <Testimonials />
       <Footer />
     </div>
   );
