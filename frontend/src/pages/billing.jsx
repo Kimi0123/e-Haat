ad6ab9d50b9b256598143729d367e7eae9ba3a86
sushmitaa07 <sushmitapandey881@gmail.com>
2025-07-18 14:08:16 +0545

    initial commit

--- frontend/src/pages/billing.jsx
+++ frontend/src/pages/billing.jsx
@@ -35,7 +35,7 @@ export default function BillingPage() {
         <Navbar/>
      
 
-      <div className="max-w-6xl mx-auto p-6 bg-white text-black">
+      <div className="max-w-6xl mx-auto p-6 pt-20 bg-white text-black">
         <h2 className="text-3xl font-bold mb-10">Billing Details</h2>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
