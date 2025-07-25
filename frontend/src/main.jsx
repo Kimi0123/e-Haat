4b4e0180b913a85384e771ee0723a3fc327f7ae3
sushmitaa07 <sushmitapandey881@gmail.com>
2025-07-25 22:51:56 +0545

    changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./AuthContext";
+import { NotificationProvider } from "./NotificationContext";
+import { CartProvider } from "./CartContext";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter> {/* ✅ Router is here */}
-      <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <NotificationProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </NotificationProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
